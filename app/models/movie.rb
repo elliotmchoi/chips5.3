7bcd737fe9c2001066de0e45efc4a55d5927a8b7
Elliot Choi <elliot.choi@berkeley.edu>
2020-10-24 01:27:58 +0000

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,13 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 R)
   end
+  
+  def self.with_ratings(ratings_list)
+    if ratings_list == nil
+      Movies.all
+    else
+      Movies.where()
+    end
+  end
       
 end
